1aa3366c9366f05319cc5314b0a9c7c08e94e09e
Aziz Madaminov <madaminovaziz@mail.ru>
2024-06-06 16:34:03 +0300

    modified:   client/src/Components/Services/ServiceCard.tsx

--- client/src/Components/Services/ServiceCard.tsx
+++ client/src/Components/Services/ServiceCard.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import { IServiceCardProps } from './interface';
-import { useState } from 'react';
 
 export const ServiceCard: React.FC<IServiceCardProps> = ({
 	text,
